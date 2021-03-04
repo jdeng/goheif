ae9711d8fa1708bf5eefc3a2575f281e4d567488
Do Quoc Huy <doquochuy189@gmail.com>
2021-03-04 13:21:20 +0700

    set MAX_PREFIX

--- libde265/libde265/cabac.cc
+++ libde265/libde265/cabac.cc
@@ -416,8 +416,6 @@ int  decode_CABAC_TR_bypass(CABAC_decoder* decoder, int cRiceParam, int cTRMax)
 }
 
 
-#define MAX_PREFIX 32
-
 int  decode_CABAC_EGk_bypass(CABAC_decoder* decoder, int k)
 {
   int base=0;
